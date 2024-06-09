4c773d08fb44d73469dd85176ebf58c39939a6eb
Angel Esparza <119971614+AEsparza722@users.noreply.github.com>
2024-06-09 14:11:06 -0700

    Kickball and sprint

--- Assets/Scripts/IAController.cs
+++ Assets/Scripts/IAController.cs
@@ -14,6 +14,7 @@ public class IAController : Agent
     Rigidbody rb;
     public bool isTraining = false;
     float moveX;
+    float currentSpeed;
 
     public RayPerceptionSensorComponent3D rayPerceptionSensor; // Referencia al componente del sensor
 
@@ -42,24 +43,54 @@ public class IAController : Agent
     public override void OnEpisodeBegin()
     {
         GameManager.instance.RestartGame();
+        rb.velocity = Vector3.zero; // Reinicia la velocidad del rigidbody
+        moveX = 0f; // Reinicia el movimiento
+        currentSpeed = IASpeed; // Reinicia la velocidad
     }
 
     public override void OnActionReceived(ActionBuffers actions)
     {
-        moveX = actions.ContinuousActions[0];
+        var continuousActions = actions.ContinuousActions;
+        if (continuousActions.Length >= 3)
+        {
+            moveX = continuousActions[0];
+            float sprintAction = continuousActions[1];
+            currentSpeed = sprintAction > 0.5f ? IASpeed * 2f : IASpeed;
+            float KickAction = continuousActions[2];
+           
+            if(KickAction == 1)
+            {
+                KickBall();            
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Se esperaban al menos 2 acciones continuas.");
+        }
     }
 
     public override void Heuristic(in ActionBuffers actionsOut)
     {
         float MoveX = 0;
-        MoveX = Input.GetAxisRaw("Horizontal");
-        ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
-        continuousActions[0] = MoveX;
-    }
+        float sprintAction = Input.GetKey(KeyCode.LeftShift) ? 1f : 0f;
+        float kickAction = Input.GetMouseButtonDown(0) ? 1f : 0f;
 
+        ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
+        if (continuousActions.Length >= 3)
+        {
+            continuousActions[0] = MoveX;
+            continuousActions[1] = sprintAction;
+            continuousActions[2] = kickAction;
+        }
+        else
+        {
+            Debug.LogWarning("El array de acciones continuas es demasiado pequeño.");
+        }
+    }
     private void Update()
     {
-        Vector3 movement = transform.right * moveX * IASpeed;
+        Vector3 movement = transform.right * moveX * currentSpeed;
+        Debug.Log("Current Speed: " + currentSpeed);
         rb.velocity = movement;
     }
 
@@ -73,6 +104,25 @@ public class IAController : Agent
         if (collision.gameObject.CompareTag("Ball"))
         {
             this.AddReward(0.5f);
+        }
+    }
+
+    void KickBall()
+    {
+        Debug.Log("KickBall 1");
+        Collider[] colliders = Physics.OverlapSphere(transform.position, 2.5f);
+
+        if (colliders.Length != 0)
+        {
+            foreach (Collider collider in colliders)
+            {
+                if (collider.CompareTag("Ball"))
+                {
+                    Vector3 forceDir = collider.transform.position - transform.position;
+                    collider.GetComponent<Rigidbody>().AddForce(forceDir.normalized * 15f, ForceMode.Impulse);
+                    this.AddReward(0.25f);
+                }
+            }
         }
     }
 }
