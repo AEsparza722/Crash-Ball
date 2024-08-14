645cc1d774312ec33db48201ce03d9d3713da2fd
Angel Esparza <119971614+AEsparza722@users.noreply.github.com>
2024-08-13 17:14:30 -0700

    Cannon explosion

--- Assets/Scripts/RotateCannon.cs
+++ Assets/Scripts/RotateCannon.cs
@@ -11,6 +11,18 @@ public class RotateCannon : MonoBehaviour
     public bool canShot = false;
     GameObject ballContainer;
 
+    [SerializeField] ParticleSystem shootCannonExplosion;
+    [SerializeField] Material explosionMaterial;
+
+    float startValueExplosion = 0f;
+    float endValueExplosion = 1f;
+    float durationExplosion = 1f;
+    float currentValueExplosionDark;
+    float currentValueExplosionClear;
+
+    IEnumerator dissolveClear;
+    IEnumerator dissolveDark;
+
     private void Awake()
     {
         shootingDirection = transform.Find("Barrel");
@@ -30,9 +42,17 @@ public class RotateCannon : MonoBehaviour
     IEnumerator ShootBall()
     {
         canShot = false;
+        shootCannonExplosion.Play();
+        if (dissolveClear != null && dissolveDark != null)
+        {
+            StopCoroutine(dissolveClear);
+            StopCoroutine(dissolveDark);
+        }
+        dissolveClear = DissolveExplosionClear();
+        StartCoroutine(dissolveClear);
         Quaternion initialRotation = shootingDirection.rotation;
         shootingDirection.Rotate(0, UnityEngine.Random.Range(-20f, 20f), 0); //= Quaternion.Euler(0, UnityEngine.Random.Range(-30f,30f), 0);
-        Vector3 shootDirForce = shootingDirection.forward * 10f;
+        Vector3 shootDirForce = shootingDirection.forward * 20f;
         GameObject tempBall = Instantiate(ball, shootingDirection.position, Quaternion.identity, ballContainer.transform);
         tempBall.GetComponent<Rigidbody>().AddForce(shootDirForce, ForceMode.Impulse);
         shootingDirection.rotation = initialRotation;
@@ -46,6 +66,47 @@ public class RotateCannon : MonoBehaviour
         transform.Rotate(new Vector3(0, rotationSpeed, 0));
     }
 
-    
-    
+    IEnumerator DissolveExplosionClear()
+    {
+        currentValueExplosionClear = 0;
+        currentValueExplosionDark = 0;
+        currentValueExplosionClear = endValueExplosion;
+        explosionMaterial.SetFloat("_DissolveClear", currentValueExplosionClear);
+        explosionMaterial.SetFloat("_DissolveDark", currentValueExplosionDark);
+        float elapsedTime = 0;
+
+        while (elapsedTime < durationExplosion)
+        {
+            explosionMaterial.SetFloat("_DissolveClear", currentValueExplosionClear);
+            elapsedTime += Time.deltaTime;
+            float T = elapsedTime / durationExplosion; //factor de interpolacion
+            currentValueExplosionClear = Mathf.Lerp(startValueExplosion, endValueExplosion, T);
+            if (elapsedTime >= durationExplosion / 2 && currentValueExplosionDark == startValueExplosion)
+            {
+                dissolveDark = DissolveExplosionDark();
+                StartCoroutine(dissolveDark);
+            }
+            yield return null;
+        }
+        
+
+    }
+
+    IEnumerator DissolveExplosionDark()
+    {
+        float elapsedTime = 0;
+        currentValueExplosionDark = endValueExplosion;
+
+        while (elapsedTime < durationExplosion)
+        {
+            explosionMaterial.SetFloat("_DissolveDark", currentValueExplosionDark);
+            elapsedTime += Time.deltaTime;
+            float T = elapsedTime / durationExplosion; //factor de interpolacion
+            currentValueExplosionDark = Mathf.Lerp(startValueExplosion, endValueExplosion, T);
+
+            yield return null;
+        }
+        
+    }
+
 }
